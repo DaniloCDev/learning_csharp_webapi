a7b183a6863d60936b5517bec006d6075ffc9131
ItKarma <loppesofc@gmail.com>
2025-01-20 12:36:14 -0300

    add: add swagger and config

--- Program.cs
+++ Program.cs
@@ -4,21 +4,42 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+
+// Configura o repositório
 builder.Services.AddScoped<PostRepository>();
 
-// Configure MongoDB settings with both connection string and database name
+// Configura as configurações do MongoDB
 builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection("MongoDbSettings"));
 
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// Configura o Swagger
+builder.Services.AddSwaggerGen(options =>
+{
+    // Config Swagger
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "My notes/posting",
+        Version = "v1",
+        Description = "API para acessar , criar, e deletar , posts armazenados no MongoDB"
+    });
+});
+
+// Adiciona suporte à API OpenAPI (Swagger)
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure o pipeline de requisições HTTP.
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
-    app.MapOpenApi();
+    
+    // Habilita o Swagger na versão de desenvolvimento
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API notes/posting");
+        c.RoutePrefix = string.Empty; // Define a URL raiz para o Swagger UI
+    });
 }
 
 app.UseHttpsRedirection();
